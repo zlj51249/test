8e95090f54288a45bb6ac4f1fcddcd9b87c77616
michael <918543020@qq.com>
2017-08-16 11:38:16 +0800

    new hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello Python world!")
+message="Hello Python world!"
+print(message);
+
+message="Hello Python Crash Course world!"
+print(message);
+
+message='hello python aaaaaa'
+print(message);
